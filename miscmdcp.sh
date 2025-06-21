6c1f268abd6af59629aa114eabba3cdc21004cfc
hirujc-01 <hiranvinaayak04+1@gmail.com>
2025-06-21 19:56:55 +0800

    Update miscmdcp.sh

--- miscmdcp.sh
+++ miscmdcp.sh
@@ -1,12 +1,1 @@
-mkdir -p test_data
-cd test_data
-
-wget https://github.com/google-coral/pycoral/raw/main/test_data/parrot.jpg
-wget https://github.com/google-coral/pycoral/raw/main/test_data/inat_bird_labels.txt
-wget https://github.com/google-coral/pycoral/raw/main/test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite
-
-cd ..
-python3 classify_image.py \
-  --model test_data/mobilenet_v2_1.0_224_inat_bird_quant_edgetpu.tflite \
-  --labels test_data/inat_bird_labels.txt \
-  --input test_data/parrot.jpg
+wget https://github.com/google-coral/pycoral/releases/download/v2.1.0/pycoral-2.1.0-cp311-cp311-linux_aarch64.whl
